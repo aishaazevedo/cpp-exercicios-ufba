c5364abcc54b310d5f1b3b6902b5b48fee4e8160
aishaazevedo <azevedoaisha@gmail.com>
2025-09-07 20:27:54 -0300

    Create avatar.cpp
    Exercício resolvido em C++ do curso UFBA - GruPro

---
+++ avatar.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    vector<int> habilidades(N);  
+    vector<int> identificadores(N);  
+
+    for (int i = 0; i < N; ++i) {
+        cin >> habilidades[i];
+    }
+
+    for (int i = 0; i < N; ++i) {
+        cin >> identificadores[i];
+    }
+    int Y;
+    cin >> Y;  
+
+    bool encontrado = false;
+
+    for (int i = 0; i < N; ++i) {
+        if (habilidades[i] == Y) {
+            if (encontrado) {
+                cout << " ";  
+            }
+            cout << identificadores[i];
+            encontrado = true;
+        }
+    }
+    if (!encontrado) {
+        cout << "Nenhum";
+    }
+
+    cout << endl;
+
+    return 0;
+}
